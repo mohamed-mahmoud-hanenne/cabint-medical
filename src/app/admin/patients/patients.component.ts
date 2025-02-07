8bf83955cddc12e10eebd4380e58ccb5fb56abf4
mohamed-mahmoud-hanenne <medhanenne38@gmail.com>
2025-02-07 11:58:36 +0100

    new cmt

--- src/app/admin/patients/patients.component.ts
+++ src/app/admin/patients/patients.component.ts
@@ -1,11 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PatientService } from '../../services/patient.service';
+import { Patient } from '../../models/patient';
+import { Observable } from 'rxjs';
+import { error } from 'console';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-patients',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './patients.component.html',
   styleUrl: './patients.component.scss'
 })
-export class PatientsComponent {
+export class PatientsComponent implements OnInit{
 
+  patients: Patient[] = []
+  constructor(private patientService: PatientService){}
+
+  ngOnInit(): void {
+    this.getPatient()
+  }
+
+  getPatient(){
+    this.patientService.getAllPatients().subscribe(
+      (data:Patient[]) =>{
+        this.patients = data
+      },
+      (error) =>{
+        console.error('Erreur de chargement des patients', error);
+      }
+    )
+  }
+
+  openPatientForm(patient: Patient = {nom: '', prenom: '', adresse: '',telephone:''}){
+    Swal.fire({
+      title: patient.id ? 'Modifier patient' : 'Ajouter patient',
+      html: `
+      <input id="nom" class="swal2-input" placeholder="Nom" value="${patient.nom}">
+      <input id="prenom" class="swal2-input"  placeholder="Prénom" value="${patient.prenom}">
+      <input id="adresse" class="swal2-input"  placeholder="Adresse" value="${patient.adresse}">
+      <input id="telephone" class="swal2-input"  placeholder="Telephone" value="${patient.telephone}">
+      `,
+      showCancelButton:true,
+      confirmButtonText: patient.id ? 'Modifier' : 'Ajouter',
+      preConfirm: () => {
+        const nom = (document.getElementById('nom') as HTMLInputElement).value;
+        const prenom = (document.getElementById('prenom') as HTMLInputElement).value;
+        const adresse = (document.getElementById('adresse') as HTMLInputElement).value;
+        const telephone = (document.getElementById('telephone') as HTMLInputElement).value;
+
+        if(!nom || !prenom || !adresse || !telephone){
+          Swal.showValidationMessage('Tous les champs sont obligatoires');
+          return false;
+        }
+
+        return {id: patient.id, nom, prenom, adresse, telephone} as Patient;
+      } 
+    }).then((result) => {
+      if(result.isConfirmed){
+        if(patient.id){
+          this.patientService.updatePatient(result.value, patient.id).subscribe(() =>{
+            this.getPatient();
+            Swal.fire('Modifié!', 'Patient mis à jour.', 'success')
+          });
+        }else{
+          this.patientService.addPatient(result.value).subscribe(() =>{
+            this.getPatient();
+            Swal.fire('Ajouté!', 'Nouvel Patient ajouté.', 'success');
+          });
+        }
+      }
+    })
+  }
+
+  deletePatient(id:number){
+    if(id === undefined) return;
+    Swal.fire({
+      title: 'Êtes-vous sûr?',
+      text: "Cette action est irréversible!",
+      icon:'warning',
+      showCancelButton:true,
+      confirmButtonColor:'#3085d6',
+      cancelButtonColor:'#d33',
+      confirmButtonText:'Oui, supprimer!',
+    }).then((result) => {
+      if(result.isConfirmed){
+        this.patientService.deletePatient(id).subscribe(() => {
+          this.getPatient();
+          Swal.fire('supprimé!', 'L’utilisateur a été supprimé.','success');
+        })
+      }
+    })
+  }
+  
 }
---
+++ src/app/services/patient.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Patient } from '../models/patient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+
+  private apiUrl = 'http://localhost:8080/api/patients'
+
+  constructor(private http:HttpClient) { }
+
+  getAllPatients() : Observable<Patient[]>{
+    return this.http.get<Patient[]>(`${this.apiUrl}/allpatients`);
+  }
+
+  addPatient(patient: Patient): Observable<Patient>{
+    return this.http.post<Patient>(`${this.apiUrl}/addpatient`,patient)
+  }
+
+  updatePatient(patient: Patient, id:number): Observable<Patient>{
+    return this.http.put<Patient>(`${this.apiUrl}/${id}`,patient);
+  }
+
+  deletePatient(id:number):Observable<any>{
+    return this.http.delete(`${this.apiUrl}/${id}`)
+  }
+}
